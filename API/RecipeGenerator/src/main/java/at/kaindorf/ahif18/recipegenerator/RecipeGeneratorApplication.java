04ef7e5532f56063b3ec26e4f9063cb189a3b16b
Max Ertl <max.ertl@interad.at>
2022-05-02 11:27:06 +0200

    added post for recipes

---
+++ API/RecipeGenerator/src/main/java/at/kaindorf/ahif18/recipegenerator/RecipeGeneratorApplication.java
@@ -0,0 +1,11 @@
+package at.kaindorf.ahif18.recipegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeGeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeGeneratorApplication.class, args);
+    }
+}
